9bc256de5d124fc20b98da0426c8a3baac1dd0e0
Duncan Ramuhashi <137775255+DuncanRamuhashi@users.noreply.github.com>
2026-09-14 13:49:28 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Back_to_Basics_C__Programming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
